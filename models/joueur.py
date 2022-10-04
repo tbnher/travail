48cfe4b7b1e8e2d8f47b9b6d2c893d0739582ad1
tbnher <natfon20@gmail.com>
2022-10-04 11:25:21 +0200

    t

--- models/joueur.py
+++ models/joueur.py
@@ -1,10 +1,18 @@
 from odoo import api, fields, models
 
 class Joueur(models.Model):
-    _name = "joueur"
+    _name = "futsal.joueur"
     _description = "Joueur de Futsal"
 
     name = fields.Char(string='Name')
+    familyname = fields.Char(string='FamilyName')
     age = fields.Integer(string='Age')
-    level = fields.Selection([('junior','Junior'),('adult','Adult')],string='Niveau')
-    gender = fields.Selection([('homme','Homme'),('femme','Femme')],string='Genre')
+    level = fields.Selection([('junior','Junior'),('adulte','Adulte')],string='Niveau')
+    gender = fields.Selection([('homme  ','m'),('femme','f')],string='Genre')
+    attaque = fields.Integer(String='Attaque')
+    defense = fields.Integer(String='Défense')
+    physique = fields.Integer(String='Physique')
+    technique = fields.Integer(String='Technique')
+    endurance = fields.Integer(String='Endurance')
+    forme = fields.Integer(String='Forme')
+
